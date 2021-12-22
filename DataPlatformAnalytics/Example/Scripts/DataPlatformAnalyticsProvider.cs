25940d83672ae2284ee9c5c39b828dd60c202c1a
alexandr.bashkirev <aleksander.bashkiriev@playgendary.com>
2021-12-22 12:40:50 +0300

    пример использования

---
+++ DataPlatformAnalytics/Example/Scripts/DataPlatformAnalyticsProvider.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace SberGames.DataPlatform.Example
+{
+    public class DataPlatformAnalyticsProvider
+    {
+        private static DataPlatformAnalyticsProvider _instance = null;
+        
+        private DataPlatformAnalytics dataPlatformAnalytics = null;
+
+        public static DataPlatformAnalyticsProvider Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DataPlatformAnalyticsProvider();
+                    _instance.Initialize();
+                }
+
+                return _instance;
+            }
+        }
+        
+        private void Initialize()
+        {
+            var dataPlatformSettings = Resources.Load<DataPlatformSettings>("DataPlatformSettings");
+            
+            dataPlatformAnalytics = new DataPlatformAnalytics();
+            dataPlatformAnalytics.Initialize(dataPlatformSettings.ApiKey, dataPlatformSettings.BaseUri);
+            
+            dataPlatformAnalytics.SetUserProperty("user_id", "example_user");
+        }
+        
+        public void SendButton1ClickedEvent()
+        {
+            EventData eventData = new EventData("button_1_clicked")
+                .AddData("some_param", "some_value");
+            dataPlatformAnalytics.SendEvent(eventData);
+        }
+        
+        public void SendButton2ClickedEvent()
+        {
+            EventData eventData = new EventData("button_2_clicked")
+                .AddData("some_param", "some_value");
+            dataPlatformAnalytics.SendEvent(eventData);
+        }
+    }
+}
