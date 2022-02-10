1bde7e02f6c1ff0e3fa6e53b857f83ae144c8ce0
Alexandr Serebryakov <a.serebryakov@sbergames.ru>
2022-02-10 17:37:23 +0300

    rebase conflict fix

---
+++ DataPlatformAnalytics/Core/IDataPlatformAnalitycsImpl.cs
@@ -0,0 +1,15 @@
+using SberGames.DataPlatform;
+using UnityEngine;
+using SberGames.DataPlatform.Core.Net;
+using System;
+
+namespace SberGames.DataPlatform.Core
+{
+    public interface IDataPlatformAnalyticsImpl : IDisposable
+    {
+        void SetUserProperty(string key, string value);
+        void RemoveUserProperty(string key);
+        void SendEvent(EventData eventData);
+        void StartSession();
+    }
+}
